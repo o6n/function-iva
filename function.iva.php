e5fbe832e15c6a7f8b3aad2ccb27e71d720be458
nazionlinux <amministratore@nazionlinux.com>
2015-12-03 23:56:07 +0100

    Creazione file

---
+++ function.iva.php
@@ -0,0 +1,23 @@
+<?php
+
+	function _iva($prezzo, $scorporo=false, $iva=22){
+
+		$prezzo = (is_int($prezzo)) ? $prezzo : die('ERRORE: prezzo');
+		$scorporo = (is_bool($scorporo) === true) ? $scorporo : false;
+		$iva = (is_int($iva) and ($iva >= 0 && $iva <= 100)) ? $iva : die('ERRORE: iva');
+
+		//$prezzo = (0 =< is_int($args[0]) => 100 ? $args[0] : $prezzo + ($prezzo * ($calc)));
+
+		$calc = $prezzo * ($iva / 100);
+
+		$totale = ($scorporo === true) ? $calc : $prezzo + ($calc);
+
+		$totale = round($totale, 2);
+
+		return $totale; 
+	}
+
+	echo '<p>'._iva(1000).'</p>'."\n"; // restituirà il prezzo compreso d'iva
+	echo '<p>'._iva(1000,true).'</p>'."\n"; // restituirà lo scorporo dell'iva
+
+?>
